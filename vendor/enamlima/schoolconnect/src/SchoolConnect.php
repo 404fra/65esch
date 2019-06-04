908ee7a1fdcbde198434147d22471e0b2316f664
404fra <404fra@gmail.com>
2019-06-04 10:59:31 +0700

    update 04062019

---
+++ vendor/enamlima/schoolconnect/src/SchoolConnect.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Enamlima\Schoolconnect;
+
+/**
+ * Class SchoolConnect.
+ */
+class SchoolConnect
+{
+  /**
+   * The Laravel admin version.
+   *
+   * @var string
+   */
+  const VERSION = '1.0.0';
+
+}
